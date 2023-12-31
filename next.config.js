9c40b4107aa0804e612bcccdf252223e4f7fbc9f
Jingnan Si <jingnan.si@gmail.com>
2023-12-31 08:39:16 -0800

    fix production api url

--- next.config.js
+++ next.config.js
@@ -1,22 +1,25 @@
-/** @type {import('next').NextConfig} */
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants')
 let API_URL = 'http://127.0.0.1:5000'
 
-if (process.env.API_URL) {
-  API_URL = process.env.API_URL
+module.exports = async (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    API_URL = 'http://quant_exchange_api:5000'
+  }
+
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+    output: 'standalone',
+
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: `${API_URL}/:path*`,
+        },
+      ]
+    },
+  }
+
+  return nextConfig
 }
-
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${API_URL}/:path*`,
-      },
-    ]
-  },
-}
-
-module.exports = nextConfig
